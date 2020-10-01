7d17095b1319af6a8d2a4f00f4ba36f29b8c5dfa
Mori <moriakaice@gmail.com>
2020-10-01 21:43:07 +0200

    Download board catalog

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const fs = require('fs')
+const axios = require('axios')
+
 const board = process.argv.slice(2).shift()
 
 const createDir = (path) => fs.existsSync(path) || fs.mkdirSync(path)
 
+const downloadThread = async (board, thread) => {
+  createDir(`${__dirname}/dl/${board}/${thread}`)
+}
+
+const run = async (board) => {
+  createDir(`${__dirname}/dl/${board}`)
+
+  const catalogHtml = (await axios.get(`https://boards.4channel.org/${board}/catalog`)).data
+  const catalog = JSON.parse(catalogHtml.split('var catalog = ').pop().split(';var style_group').shift())
+  const threads = Object.keys(catalog.threads)
+
+  for (let i = 0; i < threads.length; i++) await downloadThread(board, threads[i])
+}
+
 if (board) {
   const boardSafeName = board.toLocaleLowerCase().replace(/[^a-z0-9-_]*/g, '')
-  createDir(`${__dirname}/dl/${boardSafeName}`)
+
+  run(boardSafeName)
 }
